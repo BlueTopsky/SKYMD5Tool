1d913ca4505a6845e689b2bb76e81e8a7a45cfe0
TianXing <991526245@qq.com>
2018-04-16 13:39:13 +0800

    Add files via upload

---
+++ SKYMD5Tool.h
@@ -0,0 +1,67 @@
+//
+//  SKYMD5Tool.h
+//  MyCategory
+//
+//  Created by Topsky on 2017/1/16.
+//  Copyright © 2017年 Topsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKYMD5Tool : NSObject
+
+/**
+ *  对string进行MD5加密, 32位 小写
+ *
+ *  @param str 传入要加密的字符串
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5ForLower32Bate:(NSString *)str;
+
+/**
+ *  对string进行MD5加密, 32位 大写
+ *
+ *  @param str 传入要加密的字符串
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5ForUpper32Bate:(NSString *)str;
+
+/**
+ *  对string进行MD5加密, 16位 小写
+ *
+ *  @param str 传入要加密的字符串
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5ForLower16Bate:(NSString *)str;
+
+/**
+ *  对string进行MD5加密, 16位 大写
+ *
+ *  @param str 传入要加密的字符串
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5ForUpper16Bate:(NSString *)str;
+
+/**
+ *  对data进行MD5加密, 32位 小写
+ *
+ *  @param str 传入要加密的data
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5Lower32Digest:(NSData *)input;
+
+/**
+ *  对data进行MD5加密, 32位 大写
+ *
+ *  @param str 传入要加密的data
+ *
+ *  @return 返回加密后的字符串
+ */
++(NSString *)MD5Upper32Digest:(NSData *)input;
+
+@end
---
+++ SKYMD5Tool.m
@@ -0,0 +1,91 @@
+//
+//  SKYMD5Tool.m
+//  MyCategory
+//
+//  Created by Topsky on 2017/1/16.
+//  Copyright © 2017年 Topsky. All rights reserved.
+//
+
+#import "SKYMD5Tool.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+@implementation SKYMD5Tool
+
+#pragma mark - 32位 小写
++(NSString *)MD5ForLower32Bate:(NSString *)str{
+    
+    //要进行UTF8的转码
+    const char* input = [str UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02x", result[i]];
+    }
+    
+    return digest;
+}
+
+#pragma mark - 32位 大写
++(NSString *)MD5ForUpper32Bate:(NSString *)str{
+    
+    //要进行UTF8的转码
+    const char* input = [str UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02X", result[i]];
+    }
+    
+    return digest;
+}
+
+#pragma mark - 16位 大写
++(NSString *)MD5ForUpper16Bate:(NSString *)str{
+    
+    NSString *md5Str = [self MD5ForUpper32Bate:str];
+    
+    NSString  *string=[md5Str substringWithRange:NSMakeRange(8, 16)];
+    
+    return string;
+}
+
+
+#pragma mark - 16位 小写
++(NSString *)MD5ForLower16Bate:(NSString *)str{
+    
+    NSString *md5Str = [self MD5ForLower32Bate:str];
+    
+    NSString  *string=[md5Str substringWithRange:NSMakeRange(8, 16)];
+
+    return string;
+}
+
+#pragma mark - data 32位 小写
++(NSString *)MD5Lower32Digest:(NSData *)input {
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    
+    CC_MD5(input.bytes, (CC_LONG)input.length, result);
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
+    for (int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02x",result[i]];
+    }
+    return ret;
+}
+
+#pragma mark - data 32位 大写
++(NSString *)MD5Upper32Digest:(NSData *)input {
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    
+    CC_MD5(input.bytes, (CC_LONG)input.length, result);
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
+    for (int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02X",result[i]];
+    }
+    return ret;
+}
+
+@end
